c2493144e9171891abb70a917c06d87463b6b881
laure <laurence.bianchi@etml-es.ch>
2026-02-23 16:38:22 +0100

    complete student

--- Student.java
+++ Student.java
@@ -1,10 +1,17 @@
 public class Student {
 	private String firstName;
+        private String name;
 
 	public Student() {
 	}
 
 	public String getFirstName() {
+		return firstName;
+        }
+
+        public String getName() {
+                return name;
+
         }
 
 }
