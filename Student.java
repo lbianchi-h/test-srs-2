4b47292c7792dafffdf5e8e24b81e9c3cbfa4d1e
laure <laurence.bianchi@etml-es.ch>
2026-02-23 16:41:12 +0100

    fix student

--- Student.java
+++ Student.java
@@ -1,10 +1,14 @@
 public class Student {
 	private String firstName;
+        private int height;
 
 	public Student() {
 	}
 
 	public String getFirstName() {
+        }
+
+        public int getHeight() {
         }
 
 }
